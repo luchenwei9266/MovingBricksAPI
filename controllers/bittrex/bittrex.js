eb82499e3e48ba28cedba2b7b96f40e33c31dce6
luchenwei9266 <luchenwei9266@163.com>
2017-10-01 17:43:36 +0800

    测试接口

--- controllers/bittrex/bittrex.js
+++ controllers/bittrex/bittrex.js
@@ -5,19 +5,39 @@ const path = require('path');
 const bittrex = require('../../key');
 
 exports.getMarksummaries = wrap(function*(req, res) {
-  let marketList = [];
-  bittrex.getmarketsummaries( function( data, err ) {
-    if (err) {
-      res.send(err);
-    }
-    for( var i in data.result ) {
-      bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
-        marketList.push(ticker);
-        res.send(marketList);
-        // console.log( ticker );
-      });
-    }
-  });
+  // let marketList = [];
+  // bittrex.getmarketsummaries( function( data, err ) {
+  //   if (err) {
+  //     res.send(err);
+  //   }
+  //   for( var i in data.result ) {
+  //     bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+  //       // marketList.push(ticker);
+  //       // res.send(marketList);
+  //       console.log( ticker );
+  //     });
+  //   }
+  // });
   
+  // 
+  // 
+
+  /* 
+    获取某个交易对的当前价格，排列最前方的买卖挂单
+    bittrex.getticker({market: 'BTC-NEO'},function(data,err){
+      res.send(data);
+    })
+  */
+
+  /* 
+    获取当前账户的信息，包括区块链资产名，余额，钱包地址
+    bittrex.getbalances(function(data,err) {
+      res.send(data);
+    })
+  */
+  bittrex.getorderbook({ market : 'BTC-LTC', depth : 1, type : 'both' }, function( data, err ) {
+    res.send( data );
+  });
+
 });
 
